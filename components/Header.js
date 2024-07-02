48730c8fef874a8a553f18eaa47adcc508b515bb
Kellen Mace <kellen.mace@gmail.com>
2024-07-02 17:20:54 -0400

    Remove unneeded files, add several pages and templates

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <p>Header goes here</p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import { getWordPressProps, WordPressTemplate } from '@faustwp/core';
+import SEO from "../components/SEO";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Link from "next/link";
 
 export default function Page(props) {
-  return <WordPressTemplate {...props} />;
-}
-
-export function getStaticProps(ctx) {
-  return getWordPressProps({ ctx });
+  return (
+    <>
+      <SEO />
+      <Header />
+      <div>This is the homepage</div>
+      <br />
+      <Link href="/faust-we-made-contact">Post: Faust, We Made Contact</Link>
+      <br />
+      <Link href="/reference-page">Page: Reference Page</Link>
+      <Footer />
+    </>
+  );
 }
